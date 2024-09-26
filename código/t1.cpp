4569446da1a226194464a429e43639c3e80e8e90
fgutierrezalbornoz <francisco.gutial@gmail.com>
2024-09-25 21:40:36 -0300

    implementa caso rebalse creando una nueva página y agregando nuevo elemento a la página nueva

--- código/t1.cpp
+++ código/t1.cpp
@@ -13,16 +13,33 @@ constexpr size_t MAX_SIZE = 1024;
 constexpr size_t ELE_SIZE = sizeof(long long);
 constexpr size_t PAGE_SIZE = MAX_SIZE / ELE_SIZE;
 
+long long h(int y){
+    long long upper = (1LL << 24) - 1;
+    long long num = rand() % upper;
+    return num;
+}
+
 class Page {
 public:
     long long page[PAGE_SIZE];
     int last_pos = 0;
+    int page_index;
+    long long linked_page[PAGE_SIZE];
     Page() {
         std::fill(std::begin(page), std::end(page), 0);
     }
+    void print(){
+        for (size_t j = 0; j < PAGE_SIZE; ++j) {
+            if(j%8==0){
+                std::cout<<"\n";
+            }
+            std::cout << page[j] << " ";
+        }
+        std::cout << "\n";
+        }
 };
 
-class File {
+class HashTable {
 private:
     int p;                  // Número de páginas
     int t=0;
@@ -30,10 +47,11 @@ private:
 
 public:
     // Constructor que inicializa la clase con el número de páginas
-    File(int num_pages) : p(num_pages), pages(num_pages) {
+    HashTable(int num_pages) : p(num_pages), pages(num_pages) {
         if (num_pages <= 0) {
             throw std::invalid_argument("El número de páginas debe ser positivo.");
         }
+        fill_page_index();
     }
 
     // Método para acceder a una página específica
@@ -43,6 +61,12 @@ public:
         }
         return pages[index];
     }
+    void fill_page_index(){
+        for(int i=0;i<p;++i){
+            Page& page = getPage(i);
+            page.page_index = i;
+        }
+    }
 
     // Método para obtener el número de páginas
     int getNumPages() const {
@@ -51,9 +75,24 @@ public:
     int gett() const {
         return t;
     }
+    void printHT(){
+        for(int i=0; i<p;++i){
+            std::cout<<"página n°: "<<i<<"\n";
+            pages[i].print();
+            std::cout<<"\n";
+        }
+    }
+    void expand(){
+        Page newPage = Page();
+        newPage.page_index = p;
+        p++;
+        pages.push_back(newPage);
+        //printHT();
+    }
 
     void insert(long long x){
-        long long k = h(x) % (1LL << (t+1));
+        long long k = h(x) % (1LL << t);
+        //std::cout<<"página donde debe ocurrir la inserción: "<<k<<" número de páginas actuales "<< p<< "\n";
         if (k<p){
             //insertar en pagina k
             insertInPage(x, pages[k]);
@@ -63,47 +102,59 @@ public:
             //insertar en la pagina k - 2^t
             insertInPage(x, pages[k-(1LL << t)]);
         }
+        //std::cout <<"se insertó\n";
         return;
     }
-    void insertInPage(long long x, Page P){
+    void insertInPage(long long x, Page& P){
+        //std::cout<<"ultimo insertado en pagina actual: "<<P.last_pos<<"\n";
         if (P.last_pos >= PAGE_SIZE) {
-            std::cerr << "Error: El array está lleno, no se puede insertar más elementos." << std::endl;
-            return;
+            int p_index = P.page_index;
+            if(!(p>=(1LL<<(p_index+1)))){
+                expand();
+            }
+            //std::cout<<"hay "<<p<<" páginas \n";
+            Page& P_ = getPage(1LL<<(p_index+1)-1);
+            insertInPage(x, P_);
+            //std::cerr << "Error: El array está lleno, no se puede insertar más elementos." << std::endl;
         }
         P.page[P.last_pos] = x;
-        ++P.last_pos;
+        P.last_pos++;
+        //P.print();
         return;
     }
+
 };
 
 
 
-long long h(int y){
-    long long upper = (1LL << 24) - 1; 
-    int lower = 0; 
-    long long num = rand() % (upper - lower + 1) + lower;
-    return num;
-}
+
 
 
 
 int main(){
-    int num_pages = 2;
-    File archivo(num_pages);
-     for (int page_index = 0; page_index < num_pages; ++page_index) {
+    int num_pages = 1;
+    HashTable hT(num_pages);
+
+     /* for (int page_index = 0; page_index < num_pages; ++page_index) {
             Page& pagina = archivo.getPage(page_index);
             for (size_t i = 0; i < PAGE_SIZE; ++i) {
                 pagina.page[i] = page_index * PAGE_SIZE + i; // Ejemplo de inicialización
             }
-        }
-    for (int page_index = 0; page_index < num_pages; ++page_index) {
+        } */
+    
+    for (int i=0;i<134;++i){
+        hT.insert(i);
+    }
+    hT.printHT();
+
+    /* for (int page_index = 0; page_index < num_pages; ++page_index) {
             std::cout << "Valores en la página " << page_index << ":\n";
             Page& pagina = archivo.getPage(page_index);
             for (size_t i = 0; i < PAGE_SIZE; ++i) {
                 std::cout << pagina.page[i] << " ";
             }
             std::cout << "\n";
-        }
+        } */
     return 0;
 
 }
