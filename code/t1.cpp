9663f31c1022339497fba012451a6289fe4b9c81
cstammp <cstammp@gmail.com>
2024-10-01 20:28:50 -0300

    V2

--- code/t1.cpp
+++ code/t1.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <math.h>
 #include <cstdlib>
+#include <fstream>
 
 constexpr size_t MAX_SIZE = 1024;  // Tamaño máximo de una página en bytes.
 constexpr size_t ELE_SIZE = sizeof(long long);  // Tamaño de un elemento long long (8 bytes).
@@ -11,7 +12,7 @@ constexpr size_t MAX_COST = 3; // Costo máximo promedio permitido
 
 // Función de Hash h(y)
 // Devuelve un valor random entre 0 y 2^64 − 1 para cualquier elemento
-long long h(int y){
+long long h(long long y){
     long long upper = (1LL << 24) - 1;
     long long num = rand() % upper;
     return num;
@@ -170,7 +171,7 @@ public:
             long long k = h(value) % (1LL << (t + 1));
             if (k != index_to_expand) {
                 newPage.page[newPage.last_pos] = value;
-                newPage.last_pos++
+                newPage.last_pos++;
                 oldPage.page[i] = 0; // Marcar como movido
             }
         }
@@ -186,6 +187,11 @@ public:
 
     // Inserta un elemento en la página P. Si la página está llena, inserta en la página enlazada.
     void insertInPage(long long x, Page& P){
+        for (int i = 0; i < P.last_pos; ++i){
+            if (P.page[i] == x){
+                return;
+            }
+        }
         //std::cout<<"ultimo insertado en pagina actual: "<<P.last_pos<<"\n";
         if (P.last_pos >= PAGE_SIZE) {
             if(P.linkedPage == nullptr){
@@ -204,17 +210,19 @@ public:
 int main(){
     int num_pages = 1;
     HashTable hT(num_pages);
-    
-    for (int i = 0; i < 257; ++i){
+    std::ofstream file;
+    file.open("costo_promedio.txt");
+    long long maxnum = 400LL;
+    for (long long i = 10; i <= maxnum; ++i){
+        std::cout << i << "\n";
         hT.insert(i);
-        if (i == 127){
-            std::cout << "el costo promedio es: " << hT.searchMeanCost() << "\n";
-        }
-        if (i == 200){
-            std::cout << "el costo promedio es: " << hT.searchMeanCost() << "\n";
+        if (i%10LL == 0LL){
+            //std::cout << "el costo promedio es: " << hT.searchMeanCost() << "\n";
+            file << i << " " << hT.searchMeanCost() << "\n";
         }
     }
-    hT.printHT();
+    file.close();
+    //hT.printHT();
     std::cout << "el costo promedio es: " << hT.searchMeanCost() << "\n";
 
     return 0;
